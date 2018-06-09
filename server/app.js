8959dda53dc3cd46b85a11cac82f69cf2d9a48a8
Jarkko <jarkkohei@luukku.com>
2018-06-09 18:29:24 +0300

    Add RootQuery, export it and use it in the 'app.js' as a root query for '/graphql'-route. #8

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 
+const schema = require('./schema/schema');
+
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-    
+    // schema: schema - ES6 way is shorter    
+    schema
 }));
 
 const port = 4000;
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 const graphql = reauire('graphql');
 
-const { GraphQLObjectType, GrapQLString } = graphql;
+const { GraphQLObjectType, GrapQLString, GrapQLSchema } = graphql;
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -9,4 +9,23 @@ const BookType = new GraphQLObjectType({
         name: { type: GrapQLString },
         genre: { type: GrapQLString }
     })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: {
+                id: { type: GrapQLString }
+            },
+            resolve(parent, args) {
+                // code to get data from db.
+            }
+        }
+    }
+});
+
+module.exports = new GrapQLSchema({
+    query: RootQuery
 });
