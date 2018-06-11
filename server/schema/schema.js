61e15a2056887ed1a9c0cdc9ab96756b585615bf
Jarkko <jarkkohei@luukku.com>
2018-06-11 19:18:31 +0300

    Add 'author'-type and dummy data and rootquery for it. #12

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,10 +1,10 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLInt, GraphQLSchema } = graphql;
 
 
-// Dummy data
+// Dummy data about books
 let books = [
     {   
         id: '1',
@@ -24,12 +24,42 @@ let books = [
 ];
 
 
+//  Dummy data about authors
+let authors = [
+    {
+        id: '1',
+        name: 'Author 1',
+        age: '50',
+    },
+    {
+        id: '2',
+        name: 'Author 2',
+        age: '60',
+    },
+    {
+        id: '3',
+        name: 'Author 3',
+        age: '70',
+    }
+];
+
+
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
+    })
+});
+
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
     })
 });
 
@@ -40,15 +70,25 @@ const RootQuery = new GraphQLObjectType({
         book: {
             type: BookType,
             args: {
-                id: { type: GraphQLString }
+                id: { type: GraphQLID }
             },
             resolve(parent, args) {
                 // code to get data from db.
                 return _.find(books, { id: args.id });
             }
+        },
+        author: {
+            type: AuthorType,
+            args: {
+                id: { type: GraphQLID }
+            },
+            resolve(parent, args) {
+                return _.find(authors, { id: args.id });
+            }
         }
     }
 });
+
 
 module.exports = new GraphQLSchema({
     query: RootQuery
